b076664c9049f05a4fc5f12b78bc6ebfadd68bfb
TheOneWithTheBraid <the-one@with-the-braid.cf>
2022-07-12 13:45:25 +0200

    feat: implement Web Worker support
    Signed-off-by: TheOneWithTheBraid <the-one@with-the-braid.cf>

--- hive/lib/src/backend/js/backend_manager.dart
+++ hive/lib/src/backend/js/backend_manager.dart
@@ -2,17 +2,30 @@ import 'package:hive/hive.dart';
 import 'package:hive/src/backend/storage_backend.dart';
 
 import 'native/backend_manager.dart' as native;
+import 'web_worker/backend_manager.dart' as web_worker;
 
 /// Opens IndexedDB databases
 abstract class BackendManager {
-  BackendManager._();
+  // caching the backend manager as it makes no sense to have different backends
+  // within the same application
+  static BackendManagerInterface? _manager;
 
-  // dummy implementation as the WebWorker branch is not stable yet
+  const BackendManager._();
+
   static BackendManagerInterface select(
       [HiveStorageBackendPreference? backendPreference]) {
-    switch (backendPreference) {
-      default:
-        return native.BackendManager();
+    if(_manager==null) {
+      if (backendPreference is HiveStorageBackendPreferenceWebWorker) {
+        _manager = web_worker.BackendManagerWebWorker(backendPreference.path);
+      } else if (backendPreference == HiveStorageBackendPreference.native) {
+        _manager = native.BackendManager();
+      } else if (backendPreference == HiveStorageBackendPreference.memory) {
+        _manager = native.BackendManager();
+      } else {
+        throw UnimplementedError(
+            '$backendPreference is not a known HiveStorageBackendPreference');
+      }
     }
+    return _manager!;
   }
 }
